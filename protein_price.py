e280231da487534d70f1e68592883f5424a5e36c
balrog <nona.h@seznam.cz>
2018-11-08 19:03:23 +0100

    Names of variables translated into English

--- protein_price.py
+++ protein_price.py
@@ -1,40 +1,40 @@
-volba = int(input("Do you wanna count price or nutritients? 1/2: "))
+choice = int(input("Do you wanna count price or nutritients? 1/2: "))
 program = True
 while program == True:
-    if volba == 1:
-        cena_baleni = float(input("\nEnter the price of the package in Kc: "))
-        vaha_baleni = float(input("Enter the weight of the package in grams: "))
-        vaha_porce = float(input("Enter the weight of your portion in grams: "))
-        postovne = 0
-        chci = input("Do you wanna include shipping into the price? y/n: ")
-        if chci == "y":
-            postovne = float(input("Enter the shipping price in Kc: "))
-        cena_drinku = (cena_baleni + postovne) / vaha_baleni * vaha_porce
-        print("Your drink costs: ", cena_drinku)
-        opet = input("\nDo you wanna count the price again? y/n: ")
-        if opet == "y":
-            volba = 1
-        elif opet == "n":
-            pokracovat = input("Do you wanna count nutritients? y/n: ")
-            if pokracovat == "y":
-                volba = 2
+    if choice == 1:
+        package_price = float(input("\nEnter the price of the package in Kc: "))
+        package_weight = float(input("Enter the weight of the package in grams: "))
+        portion_weight = float(input("Enter the weight of your portion in grams: "))
+        shipping = 0
+        include = input("Do you wanna include shipping into the price? y/n: ")
+        if include == "y":
+            shipping = float(input("Enter the shipping price in Kc: "))
+        portion_price = (package_price + shipping) / package_weight * portion_weight
+        print("Your drink costs: ", portion_price)
+        again = input("\nDo you wanna count the price again? y/n: ")
+        if again == "y":
+            choice = 1
+        elif again == "n":
+            count_nutri = input("Do you wanna count nutritients? y/n: ")
+            if count_nutri == "y":
+                choice = 2
             else:
                 print("The program is over.")
                 program = False
-    if volba == 2:
-        kcal_deka = float(input("\nEnter kcal for 100g of protein: "))
-        protein_deka = float(input("Enter proteins for 100g: "))
-        vaha_porce = float(input("Enter the weight of your portion in grams: "))
-        kcal_porce = kcal_deka / 100 * vaha_porce
-        protein_porce = protein_deka / 100 * vaha_porce
-        print("Your portion contains: \n", kcal_porce, " kcal \n", protein_porce, "g of proteins")
-        opet = input("\nDo you wanna count nutritients again? y/n: ")
-        if opet == "y":
-            volba = 2
-        elif opet == "n":
-            pokracovat = input("Do you wanna count a price? y/n: ")
-            if pokracovat == "y":
-                volba = 1
+    if choice == 2:
+        kcal_per_100g = float(input("\nEnter kcal for 100g of protein: "))
+        protein_per_100g = float(input("Enter proteins for 100g: "))
+        portion_weight = float(input("Enter the weight of your portion in grams: "))
+        kcal_per_portion = kcal_per_100g / 100 * portion_weight
+        protein_per_portion = protein_per_100g / 100 * portion_weight
+        print("Your portion contains: \n", kcal_per_portion, " kcal \n", protein_per_portion, "g of proteins")
+        again = input("\nDo you wanna count nutritients again? y/n: ")
+        if again == "y":
+            choice = 2
+        elif again == "n":
+            count_price = input("Do you wanna count a price? y/n: ")
+            if count_price == "y":
+                choice = 1
             else:
                 print("The program is over.")
                 program = False
